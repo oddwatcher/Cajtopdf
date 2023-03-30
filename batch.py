cf767160d5d426adef3879566d778fbd5690ff38
oddwatcher <science-35@outlook.com>
2023-03-30 23:12:39 +0800

    fixed some problem with file path

--- batch.py
+++ batch.py
@@ -5,19 +5,16 @@ class files:
     filelist = list()
     dirlist = list()
 
-def recursivefind(Dir,filelist,dirlist,lastdir):
+def absrecursivefind(Dir,filelist,dirlist):
  for i in os.listdir(Dir):
         if os.path.isdir(i):
-            if len(lastdir)!=0:
-                recursivefind(i,filelist,dirlist,lastdir+'/'+Dir)
-            else:
-                recursivefind(i,filelist,dirlist,Dir)
+            absrecursivefind(Dir+'/'+i,filelist,dirlist)
         else:
             if not i.endswith(".pdf"):
                 if not i.endswith(".html"):
                     if not i.startswith("."):
-                        filelist.append(lastdir+'/'+Dir+'/'+i)
-                        dirlist.append(lastdir+'/'+Dir+'/')
+                        filelist.append(Dir+'/'+i)
+                        dirlist.append(Dir+'/')
             
 if __name__ == "__main__":
     File = files()
@@ -28,17 +25,26 @@ if __name__ == "__main__":
     except:
         print(f"{dirs} not exist.")
         exit()
-    recursivefind(dirs,File.filelist,File.dirlist,'')
-    #print (File.filelist)
-    #print (File.dirlist)
+    absrecursivefind(dirs,File.filelist,File.dirlist)
+    print (File.filelist)
+    print (File.dirlist)
     for i,j in enumerate(File.filelist) :
         try:
+            print(f'testing file {j}\n')
             caj = CAJ(j)
-            if caj.format == "PDF" or caj.format == "KDH":
-                print(f"{j} is not a convertable file\n")
-            else:
-                output = j+".pdf"
-                os.chdir(File.dirlist(i))
-                caj.convert(output)
         except Exception:
-            print(f"{j}is not a valid file\n")
+            print(j+' is not accecptable\n')
+        else:
+                os.chdir(File.dirlist[i])
+                print(f'switch to working dir {File.dirlist[i]}')
+                if caj.format == "KDH":
+                    print(f"{j} is not a convertable file\n")
+                elif caj.format == "PDF":
+                    os.rename(j,j.replace('.caj','')+'pdf')
+                else:
+                    try:
+                        output = j+".pdf"
+                        
+                        caj.convert(output)
+                    except Exception:
+                        print(f"{j} is not a valid file\n")
